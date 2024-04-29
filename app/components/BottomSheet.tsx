16cc7cf74aca1057a7828538db16c4f1d46c309d
Andres Mariscal <mariscal.lzn@gmail.com>
2024-04-29 13:03:17 -0500

    BottomSheet added

---
+++ app/components/BottomSheet.tsx
@@ -0,0 +1,202 @@
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import {
+  Animated,
+  Dimensions,
+  KeyboardAvoidingView,
+  LayoutChangeEvent,
+  Modal,
+  PanResponder,
+  PanResponderInstance,
+  Pressable,
+  TouchableWithoutFeedback,
+  useWindowDimensions,
+  View,
+  ViewProps,
+  ViewStyle,
+} from 'react-native';
+import {useTheme} from 'react-native-paper';
+
+//#region Types
+export type BottomSheetProps = ViewProps & {
+  dismissed?: () => void;
+  isVisible: boolean;
+  setIsVisible: (isVisible: boolean) => void;
+  maxHeight: MaxHeight;
+};
+
+type MaxHeight = 25 | 50 | 85;
+//#endregion
+
+//#region Component
+const BottomSheet: React.FC<BottomSheetProps> = ({
+  children,
+  dismissed,
+  isVisible,
+  setIsVisible,
+  maxHeight,
+}) => {
+  const _sheetHeight = Dimensions.get('window').height;
+  const translateY = useRef(new Animated.Value(_sheetHeight)).current;
+  const {height} = useWindowDimensions();
+  const {colors} = useTheme();
+
+  const [panResponder, setPanResponder] = useState<PanResponderInstance | null>(
+    null,
+  );
+  const [sheetHeight, setSheetHeight] = useState(0);
+  const [_isVisible, _setIsVisible] = useState(false);
+
+  const expandContent = useCallback(() => {
+    _setIsVisible(true);
+    Animated.spring(translateY, {
+      toValue: 0,
+      useNativeDriver: true,
+    }).start();
+  }, [translateY]);
+
+  const collapseContent = useCallback(() => {
+    Animated.spring(translateY, {
+      toValue: sheetHeight,
+      useNativeDriver: true,
+      restSpeedThreshold: 100,
+      restDisplacementThreshold: 100,
+    }).start(() => {
+      _setIsVisible(false);
+      setIsVisible(false);
+      dismissed?.();
+      //Reset values to allow the bottom sheet to start over fresh
+      translateY.setValue(_sheetHeight);
+      setSheetHeight(0);
+    });
+  }, [dismissed, setIsVisible, sheetHeight, translateY, _sheetHeight]);
+
+  useEffect(() => {
+    setPanResponder(
+      PanResponder.create({
+        onStartShouldSetPanResponder: () => sheetHeight <= height * 0.5,
+        onStartShouldSetPanResponderCapture: (_, g) =>
+          sheetHeight === 0 && g.dy === 0,
+        //Only response to vertical gestures
+        onMoveShouldSetPanResponder: (_, gestureState) =>
+          Math.abs(gestureState.dy) > Math.abs(gestureState.dx),
+        //Update translateY value based on the gesture position
+        onPanResponderMove: (_, gestureState) =>
+          translateY.setValue(gestureState.dy >= 0 ? gestureState.dy : 0),
+        onPanResponderRelease: (_, gestureState) => {
+          //If the gesture ends above the threshold, the bottom sheet is dismissed
+          if (gestureState.dy > sheetHeight * 0.4) {
+            collapseContent();
+          } else {
+            //Bounce it back to the top
+            expandContent();
+          }
+        },
+      }),
+    );
+  }, [collapseContent, expandContent, translateY, sheetHeight, height]);
+
+  useEffect(() => {
+    if (isVisible) {
+      expandContent();
+    } else {
+      collapseContent();
+    }
+  }, [isVisible, translateY, expandContent, collapseContent]);
+
+  return (
+    <Modal
+      transparent
+      statusBarTranslucent
+      visible={_isVisible}
+      animationType="fade">
+      <KeyboardAvoidingView style={$container} behavior="height">
+        <TouchableWithoutFeedback onPress={() => collapseContent()}>
+          <View
+            // eslint-disable-next-line react-native/no-inline-styles
+            style={[$shadowContainer, {backgroundColor: '#00000080'}]}
+          />
+        </TouchableWithoutFeedback>
+        <View style={$sheetContainer}>
+          <Animated.View
+            {...panResponder?.panHandlers}
+            style={[
+              $sheet,
+              {
+                backgroundColor: colors.surface,
+                transform: [{translateY: translateY}],
+              },
+            ]}
+            onLayout={(e: LayoutChangeEvent) => {
+              setSheetHeight(e.nativeEvent.layout.height);
+            }}>
+            <View
+              style={[
+                $childrenContainer,
+                {
+                  maxHeight: height * (maxHeight / 100),
+                },
+              ]}>
+              <Pressable style={$dragIndicatorContainer}>
+                <View
+                  style={[$dragIndicator, {backgroundColor: colors.onSurface}]}
+                />
+              </Pressable>
+              {children}
+            </View>
+          </Animated.View>
+        </View>
+      </KeyboardAvoidingView>
+    </Modal>
+  );
+};
+
+export default BottomSheet;
+//#endregion
+
+//#region Styles
+const $container: ViewStyle = {
+  position: 'absolute',
+  bottom: 0,
+  start: 0,
+  end: 0,
+  top: 0,
+};
+
+const $shadowContainer: ViewStyle = {
+  position: 'absolute',
+  bottom: 0,
+  start: 0,
+  end: 0,
+  top: 0,
+};
+
+const $sheetContainer: ViewStyle = {
+  position: 'absolute',
+  bottom: 0,
+  start: 0,
+  end: 0,
+};
+
+const $sheet: ViewStyle = {
+  justifyContent: 'flex-end',
+  backgroundColor: 'white',
+  borderTopStartRadius: 10,
+  borderTopEndRadius: 10,
+};
+
+const $dragIndicatorContainer: ViewStyle = {
+  flexDirection: 'row',
+  justifyContent: 'center',
+  paddingVertical: 12,
+};
+
+const $dragIndicator: ViewStyle = {
+  width: 32,
+  height: 4,
+  borderRadius: 8,
+};
+
+const $childrenContainer: ViewStyle = {
+  paddingBottom: 16,
+};
+//#endregion
--- app/screens/stores/StoresScreen.tsx
+++ app/screens/stores/StoresScreen.tsx
@@ -5,6 +5,8 @@ import {ShoppingStackScreenProps} from '../../routes/ShoppingNavigator';
 import {Button, Text, TextInput} from 'react-native-paper';
 import {createOrUpdateStore, fetchStores} from './storesSlice';
 import {FlatList} from 'react-native-gesture-handler';
+import BottomSheet from '../../components/BottomSheet';
+import {View} from 'react-native';
 
 const StoresScreen: FC<ShoppingStackScreenProps<'Stores'>> = () => {
   const dispatch = useAppDispatch();
@@ -15,18 +17,35 @@ const StoresScreen: FC<ShoppingStackScreenProps<'Stores'>> = () => {
   }, [dispatch]);
 
   const [text, setText] = React.useState('');
+  const [isBottomSheetVisible, setBottomSheetVisible] = React.useState(false);
   return (
     <Screen safeAreaEdges={['top', 'bottom']}>
-      <TextInput
-        label={'Name'}
-        value={text}
-        onChangeText={_text => setText(_text)}
-      />
       <Button
-        mode="contained"
-        onPress={() => dispatch(createOrUpdateStore({id: '', name: text}))}>
-        Add
+        onPress={() => {
+          setBottomSheetVisible(true);
+        }}>
+        Open
       </Button>
+      <BottomSheet
+        maxHeight={50}
+        isVisible={isBottomSheetVisible}
+        setIsVisible={setBottomSheetVisible}>
+        <View>
+          <TextInput
+            label={'Name'}
+            value={text}
+            onChangeText={_text => setText(_text)}
+          />
+          <Button
+            mode="contained"
+            onPress={() => {
+              setBottomSheetVisible(false);
+              dispatch(createOrUpdateStore({id: '', name: text}));
+            }}>
+            Add
+          </Button>
+        </View>
+      </BottomSheet>
       <FlatList
         data={selector.stores}
         keyExtractor={item => item.id}
